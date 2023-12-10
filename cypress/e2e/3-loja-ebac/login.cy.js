8964ada542138f4329fb7657854ea6809bc4dbf0
rosampe <rosanamariapedro@outlook.com>
2023-12-10 18:32:01 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade login', () =>{
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('rpedro.teste@teste.com.br')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, rpedro.teste (não é rpedro.teste? Sair)')
+
+})
+})
