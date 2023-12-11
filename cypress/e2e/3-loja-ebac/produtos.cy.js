40ca57e5d3ac6e3314f3440088fd258fc35e3f4f
rosampe <rosanamariapedro@outlook.com>
2023-12-11 14:46:25 -0300

    teste - Deve selecionar um produto da lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -3,11 +3,17 @@
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
     });
     
     it('Deve selecionar um produto da lista', () => {
-        
-        
+        cy.get('.products > .row')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+
+            cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 });
