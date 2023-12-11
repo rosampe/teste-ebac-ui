35cd9c764447c05aedc1c92374b20e8c70281fdd
rosampe <rosanamariapedro@outlook.com>
2023-12-11 01:12:43 -0300

    alteracoes realizadas no email

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -11,21 +11,21 @@ describe('Funcionalidade login', () =>{
     });
 
     it('Deve fazer login com sucesso', () => {
-        cy.get('#username').type('rpedro.teste@teste.com.br')
+        cy.get('#username').type('rosampe.teste@teste.com.br')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, rpedro.teste (não é rpedro.teste? Sair)')
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
-        cy.get('#username').type('rpedro@teste.com.br')
+        cy.get('#username').type('rosampe@teste.com.br')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('exist')
     });
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
-        cy.get('#username').type('rpedro.teste@teste.com.br')
+        cy.get('#username').type('rosampe.teste@teste.com.br')
         cy.get('#password').type('teste321')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-error').should('contain' , 'Erro: a senha fornecida para o e-mail rpedro.teste@teste.com.br está incorreta.')
