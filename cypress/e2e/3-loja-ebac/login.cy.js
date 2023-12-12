b06196120677c261a7f8b114b031235b943489f9
rosampe <rosanamariapedro@outlook.com>
2023-12-12 17:15:35 -0300

    alteração aula 6 - Arquivo de dados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade login', () =>{
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')        
+        cy.visit('minha-conta')        
     });
 
     afterEach(() => {
@@ -14,7 +15,7 @@ describe('Funcionalidade login', () =>{
         cy.get('#username').type('rosampe.teste@teste.com.br')
         cy.get('#password').type('teste@123')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, rpedro.teste (não é rpedro.teste? Sair)')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, rosampe.teste (não é rosampe.teste? Sair)')
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
@@ -28,6 +29,22 @@ describe('Funcionalidade login', () =>{
         cy.get('#username').type('rosampe.teste@teste.com.br')
         cy.get('#password').type('teste321')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error').should('contain' , 'Erro: a senha fornecida para o e-mail rpedro.teste@teste.com.br está incorreta.')
+        cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail rosampe.teste@teste.com.br está incorreta.')
+    });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, rosampe.teste (não é rosampe.teste? Sair)')
+    });
+
+    it.only('Deve fazer login com sucesso - Usando Fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario, {log: false})
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, rosampe.teste (não é rosampe.teste? Sair)')
+        })
     });
 })
